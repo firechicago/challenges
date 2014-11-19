d515fbee69827d3b06e5e031a8e2a118c8a8d098
Chris Wand <chris.wand@gmail.com>
2014-11-19 14:09:44 -0500

    Index Page correctly rendering and reading from csv

--- news-aggregator/server.rb
+++ news-aggregator/server.rb
@@ -1,6 +1,24 @@
-# require "sinatra"
+require "sinatra"
 require "pry"
 require "CSV"
 
-links = CSV.read("links.csv")
-puts links.to_s
+
+
+def read_csv(filename)
+  raw_data = CSV.read(filename)
+  links = []
+  raw_data.each do |link|
+    new_link = {
+      title:link[0],
+      url:link[1],
+      description:link[2]
+    }
+    links << new_link
+  end
+  links.reverse
+end
+
+get "/" do
+  @links = read_csv("links.csv")
+  erb :index
+end
