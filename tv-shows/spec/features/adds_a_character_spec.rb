23ba99a4718ee8898acc988f3ec306b6cf01b77e
Chris Wand <chris.wand@gmail.com>
2014-12-22 23:01:25 -0500

    first acceptance tests

---
+++ tv-shows/spec/features/adds_a_character_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+feature 'user adds a new character', %Q{
+  As a site visitor
+  I want to add my favorite TV show characters
+  So that other people can enjoy their crazy antics
+  } do
+
+    # Acceptance Criteria:
+    # * I can access a form to add a character on a TV show's page
+    # * I must specify the character's name and the actor's name
+    # * I can optionally provide a description
+    # * If I do not provide the required information, I receive an error message
+    # * If the character already exists in the database, I receive an error message
+
+    scenario 'user adds a new character' do
+      attrs = {
+        character_name: 'Ned Stark',
+        actor_name: 'Sean Bean',
+        description: 'Lord of the North'
+      }
+      show = TelevisionShow.first
+      char = Character.new(attrs)
+
+      visit "/television_shows/#{show.id}/new"
+      fill_in 'Character Name', with: char.character_name
+      fill_in 'Actor Name', with: char.actor_name
+      fill_in 'Description', with: char.description
+      click_on 'Submit'
+
+      expect(page).to have_content 'Success'
+      expect(page).to have_content char.character_name
+      expect(page).to_not have_content char.actor_name
+      expect(page).to_not have_content char.description
+    end
+
+    scenario 'without required attributes' do
+      visit "/television_shows/#{show.id}/new"
+      click_on 'Submit'
+
+      expect(page).to_not have_content 'Success'
+      expect(page).to have_content "can't be blank"
+    end
+
+    scenario 'user cannot add a character that is already in the database'
+  end
